fcf33905f525a6c0699b7b784c91e526f1bcfa7d
mbeviz <mbeviz@DHUB3183.pc.hu.t-internal.com>
2017-06-23 12:54:41 +0200

    Proxy fixed

--- src/hu/itsh/gyakorlat/szotar/Main.java
+++ src/hu/itsh/gyakorlat/szotar/Main.java
@@ -1,9 +1,51 @@
 package hu.itsh.gyakorlat.szotar;
 
+import java.io.IOException;
+import java.net.Authenticator;
+import java.net.PasswordAuthentication;
+import java.net.URISyntaxException;
+
+import hu.itsh.gyakorlat.szotar.szotarak.OnlineSzotar;
+
 public class Main {
 
-	public static void main(String[] args) {
-		System.out.println("teszt");
-		System.out.println("test2");
+	public static void main(String[] args) throws URISyntaxException, IOException {
+		
+
+		
+		setSystemProxy("qhub03.hu.t-internal.com", 3128, "USER", "PASSWORD");
+		
+		String s = new OnlineSzotar().translateToEnglish("ddd");
+		
 	}
+	
+	public static void setSystemProxy(String proxyAddress, int proxyPort,
+            String user, String password) 
+	{
+		final String authUser = user;
+		final String authPassword = password;
+		
+		System.setProperty("proxySet", "true");
+		System.setProperty("ProxyHost", proxyAddress);
+		System.setProperty("ProxyPort", String.valueOf(proxyPort));
+		
+		System.setProperty("http.proxyHost", proxyAddress);
+		System.setProperty("http.proxyPort", String.valueOf(proxyPort));
+		
+		System.setProperty("http.proxyHost", proxyAddress);
+		System.setProperty("http.proxyPort", String.valueOf(proxyPort));
+		
+		System.setProperty("http.proxyUser", authUser);
+		System.setProperty("http.proxyPassword", authPassword);
+		
+		Authenticator.setDefault(
+		new Authenticator() {
+		 @Override
+		 public PasswordAuthentication getPasswordAuthentication() {
+		     return new PasswordAuthentication(authUser, authPassword.toCharArray());
+		 }
+		});
+
+	}
+
 }
