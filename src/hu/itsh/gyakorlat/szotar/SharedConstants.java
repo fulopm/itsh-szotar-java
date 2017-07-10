dbb392cb7c785a771ef87c6b191599001535aea6
mbeviz <mbeviz@DHUB3183.pc.hu.t-internal.com>
2017-07-10 13:50:45 +0200

    changed constants

--- src/hu/itsh/gyakorlat/szotar/SharedConstants.java
+++ src/hu/itsh/gyakorlat/szotar/SharedConstants.java
@@ -13,21 +13,14 @@ public class SharedConstants {
 	public static final String SEPARATOR_DOLLAR = "$";
 	public static final String FORMAT_YYMMDD = "yyyy-MM-dd";
 
-	public static final String[] wordClasses = { "névelö", "rövidítés", "mondat", "vt", "kifejezés", "fönév",
-			"melléknév", "elöljáró", "határozó", "definició", "vi-sz", "vt-sz", "vt/vi", "kötöszó", "névmás",
-			"vi/vt-rh", "vt-rh", "vi-rh", "vi", "vx", "vi/vt-sz", "számnév", "vx-rh"
-
-	};
-
 	public static final DateTimeFormatter DTF_YYMMDD = DateTimeFormatter.ofPattern("uuuu-MM-dd");
 
 	
 	public static Pattern REGEX_SPECIAL_CHARS = Pattern.compile("\\`|\\~|\\@|\\#|\\$|\\^|\\&|\\*|\\'|\\<|\\>|\\\"");
 	
-	public static final String[] onlineWordClassShortenEng = {"{noun}", "{vb}", "{adj.}", "{conj.}", "{pron.}", "{adv.}", "{prp.}"};
+	public static final String[] onlineWordClassShortenEng = {"{noun}", "{vb}", "{adj.}", "{conj.}", "{pron.}", "{adv.}", "{prp.}", "{num.}", "-", "-"};
 	
-	public static final String[] onlineWordClassShortenHun = {"főnév", "ige", "melléknév", "kötőszó", "névmás", "határozószó", "viszonyszó"};
-	
+	public static final String[] onlineWordClassShortenHun = {"főnév", "ige", "melléknév", "kötőszó", "névmás", "határozó", "viszonyszó", "szám", "mondat", "kifejezés"};
 	public static final String FILENAME_NGP = "NGP.xlsx";
 	public static final String FILENAME_TEMP_CSV = "table.csv";
 	
