badbacb81ef53d39b4c08234e02e6be5fadb3ef9
mbeviz <mbeviz@DHUB3183.pc.hu.t-internal.com>
2017-07-11 14:03:30 +0200

    always show on front added, cascade removed
    -cascade method removed, need to be removed from other classes

--- src/hu/itsh/gyakorlat/szotar/ui/MainContentPane.java
+++ src/hu/itsh/gyakorlat/szotar/ui/MainContentPane.java
@@ -1,12 +1,19 @@
 package hu.itsh.gyakorlat.szotar.ui;
 
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import java.beans.PropertyVetoException;
+
 import javax.swing.JDesktopPane;
 import javax.swing.JInternalFrame;
+import javax.swing.JLayeredPane;
 import javax.swing.JMenuBar;
+import javax.swing.event.InternalFrameEvent;
+import javax.swing.event.InternalFrameListener;
 
 import hu.itsh.gyakorlat.szotar.ui.windows.InternalWindow;
 
-public class MainContentPane extends JDesktopPane {
+public class MainContentPane extends JDesktopPane implements InternalFrameListener {
 
 	public MainContentPane() {
 		initComponents();
@@ -16,18 +23,63 @@ public class MainContentPane extends JDesktopPane {
 
 	}
 
-	public void addInterenalWindow(InternalWindow w) {
+	public void addInternalWindow(InternalWindow w) {
 		add(w);
+		
+		moveToFront(w);
+		
 	}
 
-	public void cascade() {
-		JInternalFrame ifs[] = getAllFrames();
-		for (int i = 0; i < ifs.length; i++) {
-			ifs[i].setBounds(i * 20, i * 20, ifs[i].getWidth(), ifs[i].getHeight());
-		}
+
+	@Override
+	public void internalFrameActivated(InternalFrameEvent arg0)
+	{
+		// TODO Auto-generated method stub
+		
 	}
-	
-	
+
+	@Override
+	public void internalFrameClosed(InternalFrameEvent arg0)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void internalFrameClosing(InternalFrameEvent arg0)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void internalFrameDeactivated(InternalFrameEvent arg0)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void internalFrameDeiconified(InternalFrameEvent arg0)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void internalFrameIconified(InternalFrameEvent arg0)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void internalFrameOpened(InternalFrameEvent arg0)
+	{
+		System.out.println("hé");
+		
+	}
+
 
 	
 
