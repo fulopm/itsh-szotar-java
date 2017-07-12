14ceff67939ccccb523e691dea7b50f5ae84ee1e
fulopm <fm.nonpersonal@icloud.com>
2017-07-12 08:47:16 +0200

    Fixed performance issues with TTS.

--- src/hu/itsh/gyakorlat/szotar/tts/TTS.java
+++ src/hu/itsh/gyakorlat/szotar/tts/TTS.java
@@ -7,31 +7,36 @@ import hu.itsh.gyakorlat.szotar.dictionaries.Dictionary;
 
 public class TTS {
 
-	String word = "";
+	private static final String VOICE_NAME = "kevin16";
+	private VoiceManager voiceManager;
+	private Voice voice;
 	
-	public TTS(String s) {
-		
-
-		String voiceName = "kevin16";
-
-		VoiceManager voiceManager = VoiceManager.getInstance();
-		Voice helloVoice = voiceManager.getVoice(voiceName);
-		Gender voiceGender = Gender.FEMALE;
-		if (helloVoice == null) {
-			System.err.println("Cannot find a voice named " + voiceName + ".  Please specify a different voice.");
+	public TTS() {
+		voiceManager = VoiceManager.getInstance();
+		voice = voiceManager.getVoice(VOICE_NAME);
+		if (voice == null) {
+			System.err.println("Cannot find a voice named " + VOICE_NAME + ".  Please specify a different voice.");
 
 			System.exit(1);
 		}
-		helloVoice.allocate();
-		helloVoice.setRate(100);
+		voice.allocate();
+		voice.setRate(100);
 		
 
-		helloVoice.speak(s);
-
-		helloVoice.deallocate();
-
+	}
+	
+	public void speak(String string) {
+		voice.speak(string);
 	}
 
+	
+	public void deallocate() {
+		voice.deallocate();
+	}
+	
+	public boolean isValid() {
+		return voice.isLoaded();
+	}
 
 	
 }
